53ed51756c76e6f52e8cfa2543d7b15ff8ca5949
ramiropol <ramiropol@gmail.com>
2020-07-28 19:47:25 -0300

    Update trello.js

--- src/_data/trello.js
+++ src/_data/trello.js
@@ -4,8 +4,8 @@ const fetch = require('node-fetch');
 // With public fallbacks for happier onboarding
 require('dotenv').config();
 const {
-  TRELLO_BOARD_URL='https://trello.com/b/Zzc0USwZ/hellotrello',
-  TRELLO_LIST_ID='5e98325d6d6bd120f2b7395f',
+  TRELLO_BOARD_URL='https://trello.com/b/tsZniOMm/prueba',
+  TRELLO_LIST_ID='5f20a9f4a518d0730c75fa7d',
   BRANCH } = process.env;
 
 
